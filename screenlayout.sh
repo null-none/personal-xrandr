af37c8f3ae2aa7e26d5f004d06a2a7949bfd5007
Dmitry Kalinin <kalinin.mitko@gmail.com>
2021-12-23 12:44:48 +0200

    Create screenlayout.sh

---
+++ screenlayout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --mode 1920x1200 --pos 3840x0 --rotate left --output HDMI3 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output VIRTUAL1 --off
